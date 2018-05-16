d470de265d9a3dc86af6f0bb6bb8e9c01799a23e
poncerapha <38406330+poncerapha@users.noreply.github.com>
2018-05-16 16:24:26 -0300

    melhorando o game

---
+++ BrickBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+    
+    
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("TRIGGER");
+        SceneManager.LoadScene("WinScreen");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("COLLISION");
+    }
+}
