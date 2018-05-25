e794972c672cab02e02f09e698e8e8774dd41363
poncerapha <38406330+poncerapha@users.noreply.github.com>
2018-05-25 12:25:03 -0300

    no message

--- BrickBreaker/Assets/Scripts/Paddle.cs
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -13,8 +13,8 @@ public class Paddle : MonoBehaviour {
 	void Update () {
         Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
 
-        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 8;
-        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f,7.5f);
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f,15.5f);
 
 
         this.transform.position = paddlePos;
